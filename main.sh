46713fe5eb62d5033113df1ec5c395aaaf1561d0
Willians Patiño <ppwj@yahoo.com>
2024-05-31 19:56:01 -0400

    Refactoring to enable reuse of set-main-env'

--- main.sh
+++ main.sh
@@ -8,37 +8,41 @@
 #  https://github.com/WojGeek/set-bash-env.git
 
 
-# En Produccion o Desarrollo
-IS_DEVELOPING=1
 
-declare_app_environment() {
-    # default location after  cloning
-    CLONEDAPP_DIR="$HOME/consoleX"
-    source $CLONEDAPP_DIR/app_env.sh
-}
+# # En Produccion o Desarrollo
+# IS_DEVELOPING=1
 
-require_helpers(){
-   source $CLONEDAPP_DIR/helpers.sh
-}
+# set_main_environment() {
+#     # default location after  cloning
+#     CLONEDAPP_DIR="$HOME/consoleX"
+#     source $CLONEDAPP_DIR/set_main-env.sh
+# }
 
-announce_end_running() {
-    if [ ! $prompt_enable ] ; then
-        echo -e "\nPlease close and re-open your console, to watch effects.\n"
-    fi 
-}
+# require_helpers(){
+#    source $CLONEDAPP_DIR/helpers.sh
+# }
+
+# announce_end_running() {
+#     if [ ! $prompt_enable ] ; then
+#         echo -e "\nPlease close and re-open your console, to watch effects.\n"
+#     fi 
+# }
 
 
-# TODO check_NodeJS
+# TODO: To enable check_NodeJS
 
 main() {
     #echo -e "Simply do more with fewer keystrokes." 
-    declare_app_environment
+
+    source "$HOME/consoleX/set_main-env.sh"
+    
     require_helpers
     # TODO:  revisar show_info
     if  [  $prompt_enable ] ; then
         customize_shell_prompt
     fi
     enable_alias_pkg_mgmt
+    doyouwish_chrome
     announce_end_running
 }
 
