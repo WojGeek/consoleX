2678a13f168998140048fd712d4bea3988bd2793
Willians Patiño <ppwj@yahoo.com>
2024-06-01 12:16:16 -0400

    Fix and debug

--- main.sh
+++ main.sh
@@ -8,42 +8,20 @@
 #  https://github.com/WojGeek/set-bash-env.git
 
 
-
-# # En Produccion o Desarrollo
-# IS_DEVELOPING=1
-
-# set_main_environment() {
-#     # default location after  cloning
-#     CLONEDAPP_DIR="$HOME/consoleX"
-#     source $CLONEDAPP_DIR/set_main-env.sh
-# }
-
-# require_helpers(){
-#    source $CLONEDAPP_DIR/helpers.sh
-# }
-
-# announce_end_running() {
-#     if [ ! $prompt_enable ] ; then
-#         echo -e "\nPlease close and re-open your console, to watch effects.\n"
-#     fi 
-# }
-
-
 # TODO: To enable check_NodeJS
 
 main() {
-    #echo -e "Simply do more with fewer keystrokes." 
 
     source "$HOME/consoleX/set_main-env.sh"
     
-    require_helpers
-    # TODO:  revisar show_info
+    #require_helpers
     if  [  $prompt_enable ] ; then
         customize_shell_prompt
     fi
     enable_alias_pkg_mgmt
     doyouwish_chrome
     announce_end_running
+    #  ❎
 }
 
 main
