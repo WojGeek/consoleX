787fd189095514bfa4508e1acd53a0ad32ca1bc5
Willians Pati~o <pwj@yahoo.com>
2024-09-19 12:29:02 -0400

    remove repeated vars

--- set_main-env.sh
+++ set_main-env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-# ruta del clone git
-CLONEDAPP_DIR="$HOME/consoleX"       ##  DO NOT CHANGE 
+# cloned app pathfiles
+CLONEDAPP_DIR="$HOME/consoleX"       		## For the sake of this app, please don't change it.
 
-console_environment=$CLONEDAPP_DIR/console.env  ## DO NOT CHANGE
-. $console_environment
+ENVIRONMENT="$CLONEDAPP_DIR/console.env"  	## For the sake of this app, please don't change it.
+source $ENVIRONMENT
 
 load_helpers() {
     # Components
@@ -29,15 +29,9 @@ allowed_packages="(deb|rpm)"
 
 
 
-# Environment console file
-ENVIRONMENT=$CLONEDAPP_DIR/console.env
-
-# get from environment status about installing Google Chrome 
-
-source $ENVIRONMENT
 
 #TODO:  Create environment file if not exist
-# file: env
+# file: console.env
 
 # En Produccion o Desarrollo
 IS_DEVELOPING=1
@@ -45,26 +39,26 @@ IS_DEVELOPING=1
 
 announce_end_running() {
     if [ ! $prompt_enable ] ; then
-        echo -e "\nPlease close and re-open your console, to watch effects.\n"
+        echo -e "\nPlease close and re-open your console, to apply changes.\n"
     fi 
 }
 
-echo -e "Main environment loaded ✅"
+echo -e "Main components loaded ✅"
 load_helpers
 
 
 
 update_prompt_status() {
     # Bash prompt
-    if grep -q "^prompt_enable=" $console_environment; then
-        sed -i "s/prompt_enable=.*/prompt_enable='false'/" $console_environment
+    if grep -q "^prompt_enable=" $ENVIRONMENT; then
+        sed -i "s/prompt_enable=.*/prompt_enable='false'/" $ENVIRONMENT
     else
-        echo "# Require to install Bash prompt by Powerline" >> $console_environment
-        echo "prompt_enable=$prompt_enable" >> $console_environment
+        echo "# Require to install Bash prompt by Powerline" >> $ENVIRONMENT
+        echo "prompt_enable=$prompt_enable" >> $ENVIRONMENT
     fi
 }
 
 update_prompt_status
-source $console_environment
+source $ENVIRONMENT
 
 
